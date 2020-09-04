035492f85d8463e104baf216360fda59ad614cab
Simon Li <orpheus+devel@gmail.com>
2020-09-04 11:55:14 +0100

    Add @actions/github

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    core.info(`The event payload: ${payload}`)
+    // github.context.issue.number
+
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
